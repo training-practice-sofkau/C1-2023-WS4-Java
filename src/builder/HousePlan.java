3812645b9d579c3b1ef70d4f08407e3c2c0b9b14
William Serna <williamsernac@hotmail.com>
2023-01-27 16:36:34 -0500

    Builder exercise

---
+++ src/builder/HousePlan.java
@@ -0,0 +1,25 @@
+package builder;
+
+public interface HousePlan {
+
+    void setWalls(String walls);
+
+    void setDoors(String doors);
+
+    void setBathrooms(String bathrooms);
+
+    void setKitchen(String kitchen);
+
+    void setRoof(String roof);
+
+    void setWindows(String windows);
+
+    void setPools(String pools);
+
+    void setBasement(String basement);
+
+    void setGarage(String garage);
+
+    void setGarden(String garden);
+
+}
