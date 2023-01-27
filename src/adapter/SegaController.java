271b4c59d3223a80b28682d619e1e206703a586e
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-27 13:46:15 -0500

    Create the interface with the common methods

---
+++ src/adapter/SegaController.java
@@ -0,0 +1,29 @@
+package adapter;
+
+public class DualSense {
+
+    public void joyStickUp(){
+        System.out.println("↑");
+    }
+
+    public void joyStickDown(){
+        System.out.println("↓");
+    }
+
+    public void joyStickLeft(){
+        System.out.println("←");
+    }
+
+    public void joyStickRight(){
+        System.out.println("→");
+    }
+
+    public void pressX(){
+        System.out.println("Action");
+    }
+
+    public void pressO(){
+        System.out.println("Jump");
+    }
+
+}
