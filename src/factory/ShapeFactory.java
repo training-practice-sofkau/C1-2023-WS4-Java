7c34a27f529725ccb81e9911ce4e13519fc89a9b
BrianSammit <briansammit.cruz@gmail.com>
2023-01-27 13:11:23 -0500

    Finish factory shape exercise

--- src/factory/ShapeFactory.java
+++ src/factory/ShapeFactory.java
@@ -1,7 +1,21 @@
 package factory;
 
-public abstract class ShapeFactory {
-    //TO-DO: Define a method that acts as the main factory
-    public abstract Shape createShape();
+public class ShapeFactory{
 
+    public Shape getShape(String shape) {
+        if (shape == null){
+           return null;
+        }
+
+        if (shape.equals("Circle")) {
+            return new Circle();
+        }else if (shape.equals("Square")) {
+                return  new Square();
+        } else if (shape.equals("Triangle")) {
+                return  new Triangle();
+        }else
+            System.out.println("Wrong arguments");
+
+        return null;
+    }
 }
