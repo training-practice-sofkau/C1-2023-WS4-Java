6ae19c52aed4156a26fb1cf9c7cdf263671e24bf
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-27 13:19:00 -0500

    Finish the Factory Exercise

--- src/factory/Main.java
+++ src/factory/Main.java
@@ -1,2 +1,15 @@
-package factory;public class Main {
+package factory;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory shape1 = new ShapeFactory("circle");
+        ShapeFactory shape2 = new ShapeFactory("square");
+        ShapeFactory shape3 = new ShapeFactory("rectangle");
+        ShapeFactory shape4 = new ShapeFactory("triangle");
+
+        shape1.draw();
+        shape2.draw();
+        shape3.draw();
+        shape4.draw();
+    }
 }
