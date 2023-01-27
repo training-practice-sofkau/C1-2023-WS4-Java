0cdfb380bc14f755361e9398009e445a05048a75
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-27 13:49:24 -0500

    Finish the Adapter exercise

--- src/adapter/Main.java
+++ src/adapter/Main.java
@@ -1,2 +1,29 @@
-package adapter;public class Main {
+package adapter;
+
+public class Main {
+    public static void main(String[] args) {
+        NesController nesController = new NesController();
+        SegaController segaController = new SegaController();
+
+        ControllerAdapter adapter = new ControllerAdapter();
+
+        cheatCode(nesController);
+        cheatCode(segaController);
+        cheatCode(adapter);
+
+    }
+
+    public static void cheatCode(ConsoleController controller){
+        System.out.println();
+        controller.arrowUp();
+        controller.arrowDown();
+        controller.arrowUp();
+        controller.arrowDown();
+        controller.arrowLeft();
+        controller.arrowRight();
+        controller.arrowLeft();
+        controller.arrowRight();
+        controller.pressB();
+        controller.pressA();
+    }
 }
