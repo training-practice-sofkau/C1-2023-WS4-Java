7a2189c4d1c68f5577e51ceb2e2f4666e859572a
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-27 13:48:59 -0500

    Creates an Adapter in order to can use the foreing class as a controller

--- src/adapter/ControllerAdapter.java
+++ src/adapter/ControllerAdapter.java
@@ -1,2 +1,36 @@
-package adapter;public class ControllerAdapter {
+package adapter;
+
+public class ControllerAdapter implements ConsoleController{
+
+    public Keyboard keyboard = new Keyboard();
+
+    @Override
+    public void arrowUp() {
+        keyboard.keyUp();
+    }
+
+    @Override
+    public void arrowDown() {
+        keyboard.keyDown();
+    }
+
+    @Override
+    public void arrowLeft() {
+        keyboard.keyLeft();
+    }
+
+    @Override
+    public void arrowRight() {
+        keyboard.keyRight();
+    }
+
+    @Override
+    public void pressA() {
+        keyboard.click();
+    }
+
+    @Override
+    public void pressB() {
+        keyboard.spaceBar();
+    }
 }
