93249db1d8c8ce52e1508b4eecf08a6b61961130
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-01-27 14:27:54 -0500

    Delete unused things

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,6 @@ import observer.Event;
 
 public class Main {
     public static void main(String[] args) {
-        Calendar calendar = new Calendar();
-        Event event1 = new Event("Graduation Sofka");
-
-        event1.addObserver(calendar);
-
-        event1.notifyObserver();
-
-        calendar.displayEvents();
-
-
-
-
 
     }
 }
